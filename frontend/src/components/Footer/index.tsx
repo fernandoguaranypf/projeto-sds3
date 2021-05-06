1c55da976e012c78cb7e10e2858453957726fe0a
Fernando Guarany <fernandoguarany@outlook.com>
2021-05-06 18:59:35 -0300

    Basic Static components

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,14 @@
+
+const Footer = () => {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">App desenvolvido por <a href="https://www.linkedin.com/in/fernando-guarany-96877852/" target="_blank" rel="noreferrer">Fernando Guarany</a></p>
+                <p className="text-light"><small><strong>Semana Spring React</strong><br />
+      Evento promovido pela escola DevSuperior: <a href="https://github.com/fernandoguaranypf/projeto-sds3" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
